dd5c1266ed17a5b92d29da0d9990aa3c9fe2a7e6
44mira <legolaslada@gmail.com>
2025-06-20 01:29:59 +0800

    BE/build: setup backend django
    build: create workflow for backend tests
    
    fix: set correct working directory for backend tests
    
    fix: use `python-version-file` for setting python version
    
    fix: actually use correct working-dir this time
    
    fix: fix paths again
    
    fix(backend-auto-tests): please work
    
    deps: add dj_rest_auth dependency
    
    deps: add django_money
    
    fix: update setting to include rest_framework.authtoken
    
    misc: add smoke test

--- backend/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from parking-backend!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ backend/parking/tests/test_smoke.py
@@ -0,0 +1,7 @@
+import pytest
+
+# TODO: Remove this file when real tests get added
+
+
+def test_smoketest():
+    assert True == (not not True)
