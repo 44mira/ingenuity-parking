8bfa2b30e4ec9fefefa5a16550ebfa9934c198a9
44mira <legolaslada@gmail.com>
2025-06-21 17:38:51 +0800

    BE/feat: make models

--- backend/parking/models.py
+++ backend/parking/models.py
@@ -1,3 +1,94 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
+from djmoney.models.fields import MoneyField
+from moneyed import PHP
 
-# Create your models here.
+
+class ParkingLocation(models.Model):
+    class Meta:
+        verbose_name = _("Parking Location")
+        verbose_name_plural = _("Parking Locations")
+
+    slots = models.IntegerField(
+        _("Slots"),
+        help_text="The number of parking slots in the location.",
+    )
+    name = models.CharField(
+        _("Name"),
+        max_length=50,
+        help_text="The name of the parking location.",
+    )
+
+
+class ParkingReservation(models.Model):
+    class Status(models.TextChoices):
+        ACTIVE = "ACTIVE", _("Active")
+        CANCELLED = "CANCELLED", _("Cancelled")
+        PAST = "PAST", _("Past")
+
+    location = models.ForeignKey(
+        ParkingLocation,
+        on_delete=models.CASCADE,
+        related_name="reservations",
+        help_text="The parking location associated with this reservation.",
+        verbose_name=_("Parking Location"),
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reservations",
+        help_text="The owner of this reservation.",
+        verbose_name=_("Owner"),
+    )
+
+    price = MoneyField(
+        verbose_name=_("Price"),
+        max_digits=14,
+        decimal_places=2,
+        default_currency=PHP,
+        help_text="The price of the reservation slot.",
+    )
+    status = models.CharField(
+        _("Status"),
+        max_length=15,
+        choices=Status.choices,
+        help_text="The status of this reservation.",
+    )
+
+    reserve_start = models.DateTimeField(
+        _("Reservation Start"),
+        help_text="The start date and time of the reservation.",
+    )
+    reserve_end = models.DateTimeField(
+        _("Reservation End"),
+        help_text="The end date and time of the reservation.",
+    )
+
+    created_at = models.DateTimeField(
+        _("Created at"),
+        auto_now_add=True,
+        help_text="The time the reservation was created.",
+    )
+
+    def save(self, **kwargs):
+        # deduct slot
+        self.location.slots -= 1
+        self.location.save()
+
+        super().save(**kwargs)
+
+    def clean(self):
+        # check for slot count
+        if self.location.slots <= 0:
+            ValidationError("There are no slots available for this parking location.")
+
+        # check for overlapping
+        overlaps = ParkingReservation.objects.filter(
+            Q(reserve_end__gte=self.reserve_start),
+            Q(reserve_start__lte=self.reserve_end),
+        )
+        if overlaps:
+            ValidationError("This reservation overlaps with an existing reservation.")
