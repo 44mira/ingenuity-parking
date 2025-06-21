69cc3be140358a9adf80078ecdfd6bb7664e9360
44mira <legolaslada@gmail.com>
2025-06-21 18:17:50 +0800

    BE/feat: register admin models

--- backend/parking/admin.py
+++ backend/parking/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from parking.models import ParkingLocation, ParkingReservation
+
+
+@admin.register(ParkingLocation)
+class ParkingLocationAdmin(admin.ModelAdmin):
+    list_display = ["name", "slots"]
+    search_fields = ["name"]
+    search_help_text = "Search locations by name."
+
+
+@admin.register(ParkingReservation)
+class ParkingReservationAdmin(admin.ModelAdmin):
+    list_display = [
+        "location__name",
+        "status",
+        "created_at",
+        "reserve_start",
+        "reserve_end",
+        "price",
+    ]
+    search_fields = ["location__name", "created_at", "status"]
+    search_help_text = "Search through reservations."
