86db862b6ccf5abcb4ca227e89feaed12ed3c4fb
44mira <legolaslada@gmail.com>
2025-06-21 17:38:28 +0800

    BE/tests: make tests tighter

--- backend/parking/tests/test_reservation.py
+++ backend/parking/tests/test_reservation.py
@@ -10,6 +10,7 @@ from parking.models import ParkingLocation, ParkingReservation
 @pytest.mark.django_db
 def test_no_reservation_overlap(parking_reservation):
     parking_location = ParkingLocation.objects.first()
+    initial_slots = parking_location.slots
 
     try:
         ParkingReservation.objects.create(
@@ -19,6 +20,8 @@ def test_no_reservation_overlap(parking_reservation):
         )
     except ValidationError:
         assert False, "Should not raise ValidationError."
+
+    assert parking_location.slots == initial_slots - 1
 
 
 @pytest.mark.django_db
@@ -36,10 +39,13 @@ def test_no_slots():
             parking_location=parking_location,
         )
 
+    assert parking_location.slots == 0
+
 
 @pytest.mark.django_db
 def test_reservation_overlap(parking_reservation):
     parking_location = ParkingLocation.objects.first()
+    initial_slots = parking_location.slots
 
     with asserts.assertRaisesMessage(
         ValidationError,
@@ -50,3 +56,15 @@ def test_reservation_overlap(parking_reservation):
             reserve_start=datetime.fromisoformat("2023-10-01T11:00:00Z"),
             reserve_end=datetime.fromisoformat("2023-10-01T13:00:00Z"),
         )
+
+    with asserts.assertRaisesMessage(
+        ValidationError,
+        "This reservation overlaps with an existing reservation.",
+    ):
+        ParkingReservation.objects.create(
+            parking_location=parking_location,
+            reserve_start=datetime.fromisoformat("2023-10-01T11:00:00Z"),
+            reserve_end=datetime.fromisoformat("2023-10-01T11:30:00Z"),
+        )
+
+    assert parking_location.slots == initial_slots
